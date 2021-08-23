6b16a70f013f54226aebd9a310bea85e101d054a
antoniomartinszup <antonio.martins@zup.com.br>
2021-08-23 20:45:38 -0300

    Adicionando modelo Autor

---
+++ src/main/java/br/com/zupacademy/antonio/casadocodigo/model/Autor.java
@@ -0,0 +1,60 @@
+package br.com.zupacademy.antonio.casadocodigo.model;
+
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "Autores")
+public class Autor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String nome;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    @Length(max = 400)
+    private String descricao;
+    private LocalDateTime dataRegistro = LocalDateTime.now();
+
+    @Deprecated
+    public Autor() {
+    }
+
+    public Autor(String nome, String email, String descricao) {
+        this.nome = nome;
+        this.email = email;
+        this.descricao = descricao;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public LocalDateTime getDataRegistro() {
+        return dataRegistro;
+    }
+}
+
