ebea60b282241964d4bd2825be51127494ffdb35
antoniomartinszup <antonio.martins@zup.com.br>
2021-08-24 18:28:36 -0300

    adicionando anotação no repository

--- src/main/java/br/com/zupacademy/antonio/casadocodigo/repository/AutorRepository.java
+++ src/main/java/br/com/zupacademy/antonio/casadocodigo/repository/AutorRepository.java
@@ -2,6 +2,8 @@ package br.com.zupacademy.antonio.casadocodigo.repository;
 
 import br.com.zupacademy.antonio.casadocodigo.model.Autor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AutorRepository extends JpaRepository<Autor, Long> {
 }
